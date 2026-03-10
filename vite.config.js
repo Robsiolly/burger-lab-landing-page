d19e8a7a1a41ffcdceb4afa301d94f9fd07f9ab3
dev <dev@local.local>
2026-03-10 17:23:14 -0300

    feat: setup PWA capabilities using vite-plugin-pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'SIOLLY BURGER',
+        short_name: 'Siolly Burger',
+        description: 'Hambúrguer artesanal feito para quem busca experiência, sabor e qualidade.',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: '/favicon.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
 })
